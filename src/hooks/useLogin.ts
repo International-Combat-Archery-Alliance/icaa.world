5a701373a08bb49ce8efa4225c3b9cfa80a64a66
Andrew Mellen <andrew_mellen@icloud.com>
2025-08-27 22:28:01 -0400

    conditionally show admin stuff only if logged in

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,7 +1,15 @@
 import { useLoginQueryClient } from '@/context/loginQueryClientContext';
 
-export default function useLogin() {
+export function useLogin() {
   const client = useLoginQueryClient();
 
   return client.useMutation('post', '/login/google');
+}
+
+export function useLoginUserInfo() {
+  const client = useLoginQueryClient();
+
+  return client.useQuery('get', '/login/google/userInfo', {
+    credentials: 'include',
+  });
 }
