ab9f2384d3915304cbde54bd1ba3c65884e95261
Andrew Mellen <andrew_mellen@icloud.com>
2025-08-29 16:37:17 -0400

    repo cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import OfficialRules from './pages/OfficialRules';
 import Events from './pages/Events';
 import NewsItem1 from './pages/news/NewsItem1';
 import NewsItem2 from './pages/news/NewsItem2';
-import Registration from './pages/Registration';
-import RegistrationsTablePage from './pages/RegistrationsTablePage';
 import Contact from './pages/Contact';
 import AdminPage from './pages/Admin';
 import { EventQueryClientProvider } from './context/eventQueryClientContext';
@@ -22,6 +20,7 @@ import { LoginQueryClientProvider } from './context/loginQueryClientContext';
 import AdminOnlyRoute from './components/auth/AdminOnlyRoute';
 import Sidebar from './components/Sidebar';
 import { UserInfoContextProvider } from './context/userInfoContext';
+import OurCommunities from './pages/OurCommunities';
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -46,10 +45,14 @@ const App = () => {
                         path="/official-rules"
                         element={<OfficialRules />}
                       />
-                      {/*<Route
+                      <Route
                         path="/our-communities"
-                        element={<OurCommunities />}
-                      />*/}
+                        element={
+                          <AdminOnlyRoute>
+                            <OurCommunities />
+                          </AdminOnlyRoute>
+                        }
+                      />
                       <Route path="/events" element={<Events />} />
                       <Route
                         path="/events/:eventId/register-free-agent"
@@ -58,11 +61,6 @@ const App = () => {
                       <Route
                         path="/events/:eventId/register-team"
                         element={<EventRegistrationTeam />}
-                      />
-                      <Route path="/registration" element={<Registration />} />
-                      <Route
-                        path="/registrations-table"
-                        element={<RegistrationsTablePage />}
                       />
                       <Route path="/contact" element={<Contact />} />
                       <Route
