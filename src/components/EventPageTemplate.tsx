126a91c7aad05d04bce564fa9f869cf5b426b373
Andrew Mellen <andrew_mellen@icloud.com>
2025-10-24 13:30:49 -0400

    Fix rules embed (#101)
    Also updates the rules doc to the latest version

--- src/components/EventPageTemplate.tsx
+++ src/components/EventPageTemplate.tsx
@@ -347,7 +347,7 @@ function EventDetailsTemplate({ event }: { event: Event }) {
           <CardContent>
             {event.rulesDocLink ? (
               <iframe
-                src={`/docs/${event.rulesDocLink}`}
+                src={`https://drive.google.com/viewerng/viewer?embedded=true&url=https://icaa.world/docs/${event.rulesDocLink}`}
                 className="w-full h-[50vh] bg-white border-0 rounded-xl"
               />
             ) : (
