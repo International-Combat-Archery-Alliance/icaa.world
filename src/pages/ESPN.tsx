c6e30175e8b168e42a434a7ed5478882ef5f7cda
Camwell55 <camcardwell5@gmail.com>
2026-07-20 11:36:28 -0400

    adjust positions

--- src/pages/ESPN.tsx
+++ src/pages/ESPN.tsx
@@ -33,7 +33,7 @@ const matches = [
             firstName: 'Nate',
             lastName: 'Langh',
             number: '3',
-            position: 'Flex',
+            position: 'Rear Guard',
             city: 'Boston',
             experience: '3',
           },
@@ -191,7 +191,7 @@ const matches = [
             firstName: 'Tim',
             lastName: 'Ahong',
             number: '21',
-            position: 'Flex',
+            position: 'Forward',
             city: 'Toronto',
             experience: '7',
           },
@@ -219,7 +219,7 @@ const matches = [
             firstName: 'Christina',
             lastName: 'Laconsay',
             number: '14',
-            position: 'Flex',
+            position: 'Rear Guard',
             city: 'Toronto',
             experience: '4',
           },
@@ -236,10 +236,10 @@ const matches = [
           {
             imageUrl:
               'https://assets.icaa.world/578f8d21-5ca0-4287-9b39-b881206be767.jpg',
-            firstName: 'Sim',
-            lastName: 'Singh', // Simran Singh
+            firstName: 'Simran',
+            lastName: 'Singh',
             number: '25',
-            position: 'Flex',
+            position: 'Centerback',
             city: 'Boston',
             experience: '6',
           },
@@ -382,9 +382,9 @@ export default function ESPNPage() {
           </CardTitle>
         </CardHeader>
         <CardContent className="text-center text-lg space-y-2">
-          <p>Friday, August 7th. 7:00 PM EST</p>
+          <p>Friday, August 7th. 3:00 PM EST</p>
           <p>Filmed live at the ESPN World Wide of Sports Complex in Orlando</p>
-          <p>Watch the broadcast on ESPN2 @ 7:00 PM!</p>
+          <p>Watch the broadcast on ESPN2 @ 3:00 PM!</p>
           <a
             href="https://www.espnwwos.com/events/competitive-sports/espn8-the-ocho/"
             target="_blank"
